08a42f09decb758b50e187ab638ee1000379f3bb
Mayur Sadashiv Desai <76611053+mayurdesai32@users.noreply.github.com>
2023-01-09 20:26:56 +0530

    Update fileError.js

--- backendNode/utils/fileError.js
+++ backendNode/utils/fileError.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const fileError = (error) => {
   fs.appendFile(
     './customError.txt',
-    `custom errorlog:- with error ${error} on time ${Date.now().toLocaleString()} \n`,
+    `error:-  ${error}  \n`,
     () => {
       console.log('error found on file ');
     }
